9ac5c7435ea14b4ad56299e665eecb327a89fad0
Daniel Kriz <147802055+Daniel908009@users.noreply.github.com>
2024-12-03 17:48:46 +0100

    added imperial system options, and finished the basic outline

--- main.py
+++ main.py
@@ -35,17 +35,30 @@ class Error_popup(Popup):
 class Main_grid(GridLayout):
 
     def get_bmi(self):
-        height = float(self.ids.height.text) / 100 # this is to convert cm to m
+        height = float(self.ids.height.text)
         weight = float(self.ids.weight.text)
-        bmi = weight / (height ** 2)
-        return bmi
+        if self.system == "Metric":
+            height = height/100 # this is to convert cm to m
+            bmi = weight / (height ** 2)
+        else:
+            bmi = 703 * (weight / (height ** 2))
+        return round(bmi, 2)
     
     def check_bmi(self):
-        if self.ids.height.text == "" or self.ids.weight.text == "" or self.ids.height.text == "0" or self.ids.weight.text == "0":
-            err_pop = Error_popup()
-            err_pop.reason("Please enter valid values")
-            return False
-        return True
+        if self.system == "Metric":
+            if self.ids.height.text == "" or self.ids.weight.text == "" or self.ids.height.text <= "122" or self.ids.height.text >= "300" or self.ids.weight.text <= "1" or self.ids.weight.text >= "300":
+                err_pop = Error_popup()
+                err_pop.reason("Please enter valid values")
+                return False
+            else:
+                return True
+        else:
+            if self.ids.height.text == "" or self.ids.weight.text == "" or self.ids.height.text >= "96" or self.ids.height.text <= "48" or self.ids.weight.text >= "700" or self.ids.weight.text <= "40":
+                err_pop = Error_popup()
+                err_pop.reason("Please enter valid values")
+                return False
+            else:
+                return True
     
     def calculate_bmi(self):
         check_BMi = self.check_bmi()
@@ -55,6 +68,25 @@ class Main_grid(GridLayout):
             res_pop = Result_popup()
             res_pop.answers(bmi)
 
+    def switch_system(self):
+        if self.system == "Metric":
+            self.system = "Imperial"
+            self.ids.system_label.text = "Switch system, current: %s" % self.system
+            self.ids.height_label.text = "Height (in) max: 96, min: 48"
+            self.ids.weight_label.text = "Weight (lbs) max: 700, min 40"
+        else:
+            self.system = "Metric"
+            self.ids.system_label.text = "Switch system, current: %s" % self.system
+            self.ids.height_label.text = "Height (cm) max: 300, min: 122"
+            self.ids.weight_label.text = "Weight (kg) max: 300, min: 1"
+
+    def switch_advanced(self):
+        if self.advanced == "False":
+            self.advanced = "True"
+            self.ids.advanced_label.text = "Advanced form, current: %s" % self.advanced
+        else:
+            self.advanced = "False"
+            self.ids.advanced_label.text = "Advanced form, current: %s" % self.advanced
 
 
 
